e6ee09a708a6cc23320c79160081f26980b1ccc3
shiva-pagadala <shivapagadala86@gmail.com>
2024-07-11 12:52:26 +0530

    first commit

---
+++ app/protected/page.tsx
@@ -0,0 +1,10 @@
+import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
+
+export default async function ProtectedPage() {
+ 
+  return (
+   <>     
+      <h2 className="font-bold text-4xl mb-4">Next steps</h2>
+      <FetchDataSteps />
+    </>);
+}
